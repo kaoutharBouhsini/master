773d08a836ef1f8cc6698337669b1a880f9ab216
abderrahim.taleb@nexia.com <abderrahim.taleb@neoxia.com>
2018-08-28 18:15:39 +0100

    params-new-design

--- src/app/home/parametrage/parametrage.component.ts
+++ src/app/home/parametrage/parametrage.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,  OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { ViewportScroller } from '../../../../node_modules/@angular/common';
 @Component({
   selector: 'app-parametrage',
   templateUrl: './parametrage.component.html',
-  styleUrls: ['./parametrage.component.css']
-})
-export class ParametrageComponent implements OnInit {
+  styleUrls: ['./parametrage.component.css'],
 
-  constructor() { }
+},
+)
+export class ParametrageComponent {
 
-  ngOnInit() {
+  constructor(private viewScroller: ViewportScroller) {
   }
 
+  scrollTo(tag : string)
+  {
+    this.viewScroller.scrollToAnchor(tag);
+  }
+
+ 
 }
